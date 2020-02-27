dbe9be8c7949af446222ea72b92917c5b1a10ff1
it-school <GITHUB it@osvita.od.ua>
2020-02-27 12:08:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNetApp192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, students!!!");
+        }
+    }
+}
